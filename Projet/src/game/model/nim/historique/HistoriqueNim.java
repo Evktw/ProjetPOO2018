09d796fd2d5dcfcdd98c021d9b9a528de84f421a
Psych3delice <m.lemainque@gmail.com>
2018-12-12 12:16:21 +0100

    Ajout Classe MoveNim2

--- Projet/src/game/model/nim/historique/HistoriqueNim.java
+++ Projet/src/game/model/nim/historique/HistoriqueNim.java
@@ -1,4 +1,30 @@
 package game.model.nim.historique;
 
+import game.model.nim.historique.MoveNim;
+import java.util.List;
+import java.util.ArrayList;
+
 public class HistoriqueNim {
+
+    /**
+     * Attributs
+     */
+    public List<MoveNim> listMoves= new ArrayList<>();          // Liste des Coups de la partie
+    public String winner;                                       // Gagnant de la partie
+    public List<String> losers = new ArrayList<>();             // Perdant(s) de la partie
+    public int gameId;                                          // Identifiant de la partie correspondant à l'historique
+
+
+    /**
+     * Constructeur
+     */
+    public HistoriqueNim(List moves, String winn,, int id ){
+        listMoves =
+
+    }
+
+
+    /**
+     * Getters & Setters
+     */
 }
--- Projet/src/game/model/nim/historique/MoveNim.java
+++ Projet/src/game/model/nim/historique/MoveNim.java
@@ -1,27 +1,54 @@
 package game.model.nim.historique;
 
-import game.model.nim.historique.MoveNim;
-
 public class MoveNim {
+
 
     /**
      * Attributs
      */
     private int spentMatches;           // Nombre d'allumettes retirées lors d'un coup
-    private int PlayerId;            // Identifiant du joueur ayant effectué le coup
+    private int remainingMatches;       // Nombre d'allumettes restantes après un coup
+    private int PlayerId;               // Identifiant du joueur ayant effectué le coup
+    private int idMove;           // Ordre des coups
+    private static int compteur = 1;           // Compteur pour les identifiants
+
 
     /**
      * Constructeur d'un coup
      */
-
-    public Coup (int nb, int id) {
-        this.spentMatches = nb;
+    public MoveNim (int nbS, int id, int nbR) {
+        this.spentMatches = nbS;
+        this.remainingMatches = nbR - this.spentMatches;
         this.PlayerId = id;
+        this.idMove= compteur;
+        compteur++;
     }
+
 
     /**
      * Getters & Setters
      */
-    
+    public int getSpentMatches() {
+        return spentMatches;
+    }
 
+    public void setSpentMatches(int spentMatches) {
+        this.spentMatches = spentMatches;
+    }
+
+    public int getPlayerId() {
+        return PlayerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        PlayerId = playerId;
+    }
+
+    public int getRemainingMatches() {
+        return remainingMatches;
+    }
+
+    public void setRemainingMatches(int remainingMatches) {
+        this.remainingMatches = remainingMatches;
+    }
 }
