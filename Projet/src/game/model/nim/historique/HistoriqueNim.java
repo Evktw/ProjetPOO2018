e5a82a89819914789822d78d389fd31091f7cbff
Psych3delice <m.lemainque@gmail.com>
2018-12-13 10:02:41 +0100

    Historique classe Historique et MoveNim OK

--- Projet/src/game/model/nim/historique/HistoriqueNim.java
+++ Projet/src/game/model/nim/historique/HistoriqueNim.java
@@ -1,7 +1,5 @@
 package game.model.nim.historique;
 
-import game.model.nim.historique.MoveNim;
-import game.model.common.Game;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -10,17 +8,17 @@ public class HistoriqueNim {
     /**
      * Attributs
      */
-    public List<MoveNim> listMoves= new ArrayList<>();          // Liste des Coups de la partie
-    public String winner;                                       // Gagnant de la partie
-    public int gameId;                                          // Identifiant de la partie correspondant à l'historique
+    private List<MoveNim> listMoves= new ArrayList<>();          // Liste des Coups de la partie
+    private String winner;                                       // Gagnant de la partie
+    private int gameId;                                          // Identifiant de la partie correspondant à l'historique
 
 
     /**
      * Constructeur
      */
-    public HistoriqueNim(ArrayList moves, String winn, int id ){
-        this.listMoves = moves;
-        this.winner = winn;
+    public HistoriqueNim(int id){
+        this.listMoves = this.getListMoves();
+        this.winner = this.listMoves.get(listMoves.size()-1).getPlayerName();
         this.gameId = id;
     }
 
@@ -29,34 +27,33 @@ public class HistoriqueNim {
      */
     public List<MoveNim> addtoListMoves(MoveNim move) {
         this.listMoves.add(move);
-        this.winner = (getPlayerById(move.PlayerId)).name;
         return listMoves;
     }
 
     /**
      * Getters & Setters
      */
-    public List<MoveNim> getListMoves() {
+    private List<MoveNim> getListMoves() {
         return listMoves;
     }
 
-    public void setListMoves(List<MoveNim> listMoves) {
+    private void setListMoves(List<MoveNim> listMoves) {
         this.listMoves = listMoves;
     }
 
-    public String getWinner() {
+    private String getWinner() {
         return winner;
     }
 
-    public void setWinner(String winner) {
+    private void setWinner(String winner) {
         this.winner = winner;
     }
 
-    public int getGameId() {
+    private int getGameId() {
         return gameId;
     }
 
-    public void setGameId(int gameId) {
+    private void setGameId(int gameId) {
         this.gameId = gameId;
     }
 }
--- Projet/src/game/model/nim/historique/MoveNim.java
+++ Projet/src/game/model/nim/historique/MoveNim.java
@@ -1,7 +1,5 @@
 package game.model.nim.historique;
 
-import java.util.List;
-
 public class MoveNim {
 
 
@@ -10,7 +8,7 @@ public class MoveNim {
      */
     private int spentMatches;                  // Nombre d'allumettes retirées lors d'un coup
     private int remainingMatches;              // Nombre d'allumettes restantes après un coup
-    private int PlayerId;                      // Identifiant du joueur ayant effectué le coup
+    private String PlayerName;                 // Identifiant du joueur ayant effectué le coup
     private final int idMove;                  // Ordre des coups
     private static int compteur = 1;           // Compteur pour les identifiants
 
@@ -18,26 +16,17 @@ public class MoveNim {
     /**
      * Constructeur d'un coup
      */
-    public MoveNim (int nbS, int id, int nbR) {
+    public MoveNim (int nbS, String name, int nbR) {
         this.spentMatches = nbS;
         this.remainingMatches = nbR - this.spentMatches;
-        this.PlayerId = id;
+        this.PlayerName = name;
         this.idMove= compteur;
         compteur++;
     }
 
     /**
-     * Méthode de récupération de l'intégralité des coups d'une partie
-     */
-    public List<MoveNim> addtoListMoves(){
-
-
-    }
-
-    /**
      * Getters & Setters
      */
-
     public int getIdMove() {
         return idMove;
     }
@@ -46,16 +35,16 @@ public class MoveNim {
         return spentMatches;
     }
 
-    public void setSpentMatches(int spentMatches) {
+    private void setSpentMatches(int spentMatches) {
         this.spentMatches = spentMatches;
     }
 
-    public int getPlayerId() {
-        return PlayerId;
+    public String getPlayerName() {
+        return PlayerName;
     }
 
-    public void setPlayerId(int playerId) {
-        PlayerId = playerId;
+    private void setPlayerName(String name) {
+        PlayerName = name;
     }
 
     public int getRemainingMatches() {
